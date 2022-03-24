001fffc7572a64262f3c3867142dbf97b230cce1
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-03-24 17:59:30 +0100

    Move content- state to App

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import {useFetch} from "../index";
+import React from "react";
 
-function Start(props) {
-    const [content, loading, error] = useFetch("http://localhost:1337/api/restaurants");
-    ;
-    if(loading) return <p>loading</p>
-    else if(error) return <p>nääj det blev feeel</p>
-    else if (content)
-    return (
-        <h2>
-            {content.data[0].attributes.name}
-        </h2>
-    );
-    else return null
+function Start({ content, loading, error }) {
+  if (loading) return <p>loading</p>;
+  else if (error) return <p>nääj det blev feeel</p>;
+  else if (content) return <h2>{content.data[0].attributes.Text_Content}</h2>;
+  else return null;
 }
 
 export default Start;
