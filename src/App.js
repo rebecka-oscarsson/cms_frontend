c51292b7a253e712728a53a91734ab8b4fd30dcc
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-03-24 17:56:00 +0100

    added states for language and content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { useState, useEffect } from "react";
 import "./normalize.css";
 import "./App.css";
-import {useFetch,
-  Header,
-  Start,
-  Main,
-  Nav 
-} from "./index";
+import { useFetch, Start, Main, Nav } from "./index";
 
 function App() {
-  const [language, setLanguage] = useState("");
+  const [language, setLanguage] = useState(
+    localStorage.getItem("language") || "en"
+  );
+  let url = `http://localhost:1337/api/pages/?locale=${language}`;
+  const [content, loading, error] = useFetch(url);
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
 
   return (
     <>
-      <Nav />
-      <Start />     
-      <Main />      
+      <Nav setLanguage={setLanguage} language={language} />
+      <Start
+        language={language}
+        content={content}
+        loading={loading}
+        error={error}
+      />
+      <Main />
     </>
   );
 }
