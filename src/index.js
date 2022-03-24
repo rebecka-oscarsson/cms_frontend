196023eabbe9faeb6cd61a9331ab9f6240b3cef9
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-03-24 17:56:54 +0100

    deleted and added components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-export {default as Nav} from './components/Nav'
-export {default as Header} from "./components/Header"
-export {default as Start} from "./components/Start"
-export {default as Link} from "./components/Link"
-export {default as Main} from "./components/Main"
-export {default as useFetch} from "./hooks/useFetch"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+export { default as Nav } from "./components/Nav";
+export { default as LanguageDropdown } from "./components/LanguageDropdown";
+export { default as Start } from "./components/Start";
+export { default as Link } from "./components/Link";
+export { default as Main } from "./components/Main";
+export { default as useFetch } from "./hooks/useFetch";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
