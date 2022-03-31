9c59d120869586cb023751aff65c0765139c0b7e
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-03-31 13:15:54 +0200

    Add standard Page component and react-markdown

---
+++ src/components/Page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+//problem: css:en kommer krascha om man byter namn på sidan
+function Page({ pageName, content, loading, error }) {
+  if (loading) return "loading";
+  if (error) return "nääj det blev feeel";
+  if (content && Object.keys(content).length > 0)
+    //det här borde inte behöva vara här. varför blir content undefined? utan att det är loading eller error?
+    return <div className={pageName}><ReactMarkdown>{content}</ReactMarkdown></div>;
+}
+
+export default Page;
