4b891ba710bafbe8795acded7a3bb68efa2b8076
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-03-31 13:25:19 +0200

    Minor fixes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,16 +3,17 @@ import { useState, useEffect } from "react";
 const useFetch = (url) => {
   const [content, setContent] = useState(null);
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchContent = async () => {
       setLoading(true);
+      setContent(null)
       setError(false); //behövs denna?
       try {
         const res = await fetch(url);
         const json = await res.json();
-        setContent(json);
+        setContent(json.data);
         setLoading(false);
       } catch (error) {
         setError(true);
