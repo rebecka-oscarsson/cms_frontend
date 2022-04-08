f325d89515fe50d5c1119c310b5c42b30cb0571f
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-08 15:07:09 +0200

    Change name of fetched content

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -6,7 +6,7 @@ function Start({ content, loading, error }) {
   if (error) return "nääj det blev feeel";
   if (content && Object.keys(content).length > 0)
     //det här borde inte behöva vara här. varför blir content undefined? utan att det är loading eller error?
-    return <div className="start"><h2>{content.Title}</h2>{content.Description}</div>;
+    return <div className="start"><h2>{content.Headline}</h2>{content.Presentation}</div>;
     else return null
 }
 
