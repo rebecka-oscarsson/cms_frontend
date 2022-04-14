fb31c78c29eafd18355dc960984e764b5b0b80a7
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-14 17:27:37 +0200

    Add startpage elements

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 //problem: css:en kommer krascha om man byter namn på sidan
 function Start({ content, loading, error }) {
+  
   if (loading) return "loading";
   if (error) return "nääj det blev feeel";
-  if (content && Object.keys(content).length > 0)
+  if (content && Object.keys(content).length > 0){console.log(content.attributes)
     //det här borde inte behöva vara här. varför blir content undefined? utan att det är loading eller error?
-    return <div className="start"><h2>{content.attributes.Headline}</h2>{content.attributes.Presentation}</div>;
-    else return null
+    return (
+      <div className="start">
+        <h2>{content.attributes.Headline}</h2>
+        <p>{content.attributes.Presentation}</p>
+        <button>{content.attributes.Button_text}</button>
+        <img src="https://res.cloudinary.com/dc39bw2wz/image/upload/v1649946049/sthlm_cc9bbd58a4.svg"></img>
+      </div>
+    );}
+  else return null;
 }
 
 export default Start;
