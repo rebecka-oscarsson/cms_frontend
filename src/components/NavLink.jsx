35912fca6baca07b5ec5de95c153ef87a7e020fe
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-14 11:47:21 +0200

    Style links :hover state

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.scss";
 
-function NavLink({ pageId, text, id, language, closeMobileMenu, onMouseEnter, onMouseLeave }) {
+function NavLink({ pageId, text, id, language, closeMobileMenu }) {
   return (
     <li key={id}>
-      <Link to={`/${pageId}?lang=${language}`} onClick={closeMobileMenu} className={styles.navlinks}>{text}</Link>
+      <Link to={pageId ==="start" ? `/?lang=${language}`: `/${pageId}?lang=${language}`} onClick={closeMobileMenu} className={styles.navlinks}>{text}</Link>
     </li>
   );
 }
