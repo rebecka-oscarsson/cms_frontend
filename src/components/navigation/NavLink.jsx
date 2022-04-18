3757af7dd09fb004c7cffcf22cdb5c78d7e17fa1
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-18 20:01:55 +0200

    Style links

--- src/components/navigation/NavLink.jsx
+++ src/components/navigation/NavLink.jsx
@@ -5,7 +5,17 @@ import styles from "./NavLink.module.scss";
 function NavLink({ pageId, text, id, language, closeMobileMenu }) {
   return (
     <li key={id}>
-      <Link to={pageId ==="start" ? `/?lang=${language}`: `/${pageId}?lang=${language}`} onClick={closeMobileMenu} className={styles.navlink}>{text}</Link>
+      <Link
+        to={
+          pageId === "start"
+            ? `/?lang=${language}`
+            : `/${pageId}?lang=${language}`
+        }
+        onClick={closeMobileMenu}
+        className={styles.navlink}
+      >
+        {text}
+      </Link>
     </li>
   );
 }
