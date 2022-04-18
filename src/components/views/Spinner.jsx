bc47a954257cd5b23d2713edf150ee844280cbd3
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-18 20:03:48 +0200

    Add alt-text

--- src/components/views/Spinner.jsx
+++ src/components/views/Spinner.jsx
@@ -3,7 +3,7 @@ import styles from "./Spinner.module.scss";
 import spinner from "../../images/spinner.gif";
 
 function Spinner() {
-    return <main className={styles.spinner}><img src={spinner} /></main>;
+    return <main className={styles.spinner}><img src={spinner} alt="loading"/></main>;
 }
 
 export default Spinner;
