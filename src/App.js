4180d2c57d4c1c2c9bb56952a5dc00ad9172b7bc
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-21 19:49:20 +0200

    Language support with React Helmet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./normalize.css";
 import "./App.scss";
 import { useFetch, PageRoutes, Nav } from "./index";
 import { useSearchParams } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   //gets the url parameters
@@ -13,10 +14,9 @@ function App() {
     searchParams.get("lang") || localStorage.getItem("language") || "en"
   );
 
-  const remoteUrl = "https://spraakteknik.herokuapp.com/api/"
-  let strapiUrl = process.env.REACT_APP_STRAPI_URL;
-  strapiUrl = remoteUrl //for testing locally with remote data
-  
+  const remoteUrl = "https://spraakteknik.herokuapp.com/api/";
+  let strapiUrl = process.env.REACT_APP_STRAPI_URL; //for testing with local instance of strapi specified in env-file
+  strapiUrl = remoteUrl; //for testing the project locally using data from remote cms, comment out if using local cms
 
   let pagesUrl = `${strapiUrl}pages/?sort=SortOrder&locale=${language}`;
   let startUrl = `${strapiUrl}start/?locale=${language}`;
@@ -28,41 +28,43 @@ function App() {
   const [contactContent, contactLoading, contactError] = useFetch(contactUrl);
   const [content, loading, error] = useFetch(pagesUrl);
 
+  
   useEffect(() => {
     localStorage.setItem("language", language);
     if (searchParams.get("lang") !== language) {
       setSearchParams({ lang: language });
     }
   }, [language, setSearchParams, searchParams]);
-  //varför tycker lintern att setSearcParams ska vara en dependency? Det är en funktion den ändras inte?
 
   return (
-    <>
-      <Nav
-        setLanguage={setLanguage}
-        language={language}
-        content={content}
-        startContent={startContent}
-        contactContent={contactContent}
-        pricingContent={pricingContent}
-      />
-    
-      <PageRoutes
-        content={content}
-        loading={loading}
-        error={error}
-        startContent={startContent}
-        startLoading={startLoading}
-        startError={startError}
-        pricingContent={pricingContent}
-        pricingLoading={pricingLoading}
-        pricingError={pricingError}
-        contactContent={contactContent}
-        contactLoading={contactLoading}
-        contactError={contactError}
-        language={language}
-      />
-    </>
+    <HelmetProvider>
+      <>
+        <Helmet htmlAttributes={{ lang: language }}></Helmet>
+        <Nav
+          setLanguage={setLanguage}
+          language={language}
+          content={content}
+          startContent={startContent}
+          contactContent={contactContent}
+          pricingContent={pricingContent}
+        />
+        <PageRoutes
+          content={content}
+          loading={loading}
+          error={error}
+          startContent={startContent}
+          startLoading={startLoading}
+          startError={startError}
+          pricingContent={pricingContent}
+          pricingLoading={pricingLoading}
+          pricingError={pricingError}
+          contactContent={contactContent}
+          contactLoading={contactLoading}
+          contactError={contactError}
+          language={language}
+        />
+      </>
+    </HelmetProvider>
   );
 }
 
--- src/components/views/Start.jsx
+++ src/components/views/Start.jsx
@@ -2,25 +2,37 @@ import React from "react";
 import styles from "./Start.module.scss";
 import { Spinner, Error } from "../../index";
 import { Link } from "react-router-dom";
-
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function Start({ content, loading, error, language }) {
-
   if (loading) return <Spinner />;
   if (error) return <Error />;
   if (content && Object.keys(content).length > 0)
     return (
+      <HelmetProvider>
       <main className={styles.start}>
+        
+        <Helmet>
+          <title>{content.attributes.Headline}</title>
+          <meta name="description" content={content.attributes.Presentation} />
+          <meta
+          name="keywords"
+          content="test"
+        />
+        </Helmet>
+
         <div className={styles.start_container}>
-        <div>
-        <h1>{content.attributes.Headline}</h1>
-        <p>{content.attributes.Presentation}</p>
+          <div>
+            <h1>{content.attributes.Headline}</h1>
+            <p>{content.attributes.Presentation}</p>
+          </div>
+          <Link to={`/pricing?lang=${language}`}>
+            <button className={styles.linkbutton}>
+              {content.attributes.Button_text}
+            </button>
+          </Link>
         </div>
-        <Link to={`/pricing?lang=${language}`}>
-        <button className={styles.linkbutton}>{content.attributes.Button_text}</button>
-        </Link>
-        </div>
-      </main>
+      </main></HelmetProvider>
     );
   else return null;
 }
