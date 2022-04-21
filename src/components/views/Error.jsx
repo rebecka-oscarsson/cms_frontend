f9ee7ae8e9414ff5aa512495e34d9d2592affb1b
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-04-21 19:36:12 +0200

    Change details in style

--- src/components/views/Error.jsx
+++ src/components/views/Error.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import styles from "./Error.module.scss";
 
 function Error() {
-    return <main className={styles.error}><h2><i className="fa fa-exclamation-triangle" aria-hidden="true" /> Error loading the page</h2></main>;
+  return (
+    <main>
+      <h2>
+        <i className="fa fa-exclamation-triangle" aria-hidden="true" /> Error
+        loading the page
+      </h2>
+    </main>
+  );
 }
 
 export default Error;
