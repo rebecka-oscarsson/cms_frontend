0f01946a0db70c9a16037f953fb5506f0d8c1c6d
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-03-31 13:27:58 +0200

    Change url for backend

--- src/App.js
+++ src/App.js
@@ -14,9 +14,8 @@ function App() {
     searchParams.get("lang") || localStorage.getItem("language") || "en"
   );
 
-  const remoteUrl = "https://spraakteknik.herokuapp.com/api/";
   let strapiUrl = process.env.REACT_APP_STRAPI_URL; //for testing with local instance of strapi specified in env-file
-  strapiUrl = remoteUrl; //for testing the project locally using data from remote cms, comment out if using local cms
+  //strapiUrl = remoteUrl; //for testing the project locally using data from remote cms, comment out if using local cms
 
   let customPagesUrl = `${strapiUrl}pages/?sort=SortOrder&locale=${language}`;
   let startUrl = `${strapiUrl}start/?locale=${language}`;
